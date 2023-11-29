472756e1ada264236b71959ae5ac57bd8f9bb7c1
DimitrisPapac <jim.papachristoudis@gmail.com>
2023-11-29 07:00:10 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::net::SocketAddr;
+use ark_bls12_381::Bls12_381;
 use tokio::time::{sleep, Duration};
+
+use crate::config::generate_setup_files;
 
 mod config;
 mod core;
@@ -9,8 +12,8 @@ mod node;
 
 #[tokio::main]
 async fn main() {
-    let num_participants = 4; // temporary value for testing purposes
-    let num_faults = (num_participants / 2) - 1; // assume maximum number of faults
+    let num_participants = 8; // temporary value for testing purposes
+    let num_faults = (num_participants >> 1) - 1; // assume maximum number of faults
     println!("Max faults: {}", num_faults);
 
     // Create local ip addresses with different ports.
@@ -22,6 +25,14 @@ async fn main() {
         })
         .collect::<Vec<_>>();
 
+    // Moved file generation here to have it execute only once.
+    let config_path = format!("config_{}_{}.txt", num_participants, num_faults);
+    let pks_path = format!("pks_{}_{}.txt", num_participants, num_faults);
+    let sks_path = format!("sks_{}_{}.txt", num_participants, num_faults);
+    let cms_path = format!("cms_{}_{}.txt", num_participants, num_faults);
+    let ips_path = format!("ips_{}_{}.txt", num_participants, num_faults);
+    generate_setup_files::<Bls12_381>(num_participants, num_faults, &config_path, &pks_path, &sks_path, &cms_path, &ips_path);
+
     // Spawn nodes.
     for i in 0..num_participants {
         let addresses = addresses.clone();
@@ -30,5 +41,5 @@ async fn main() {
         });
     }
 
-    sleep(Duration::from_millis(5_000)).await;
+    sleep(Duration::from_millis(10_000)).await;
 }
