380e2bd207942f9320d36f9b054d0089deb6f759
Simon Ochsenreither <s8siochs@stud.uni-saarland.de>
2023-11-29 20:46:09 +0100

    log parsing

--- scripts/local_run.py
+++ scripts/local_run.py
@@ -2,9 +2,10 @@ import subprocess
 import sys
 import time
 import os
+from parse import parse_files
 
 # Create a file containing only local ips.
-print("Creating ip files..")
+print("Creating ip files...")
 prefix = "127.0.0.1:"
 filename = "ips.txt"
 f = open(filename, "w")
@@ -17,19 +18,19 @@ for i in range(int(sys.argv[1])):
 
 f.close()
 
-print("Deleting old logs..")
+print("Deleting old logs...")
 for file in os.scandir("logs"):
     if file.name.endswith(".log"):
         os.unlink(file.path)
 
 # compile in release mode
-print("Compiling..")
+print("Compiling...")
 subprocess.run("cargo build --bin app --release", shell=True)
 
 # start all nodes
 subprocess.run("killall -9 app", shell=True)
 
-print("Starting nodes..")
+print("Starting nodes...")
 processes = []
 for i, ip in enumerate(ips):
     cmd = "./target/release/app {} {} &> logs/{}.log".format(i, filename, i)
@@ -37,10 +38,13 @@ for i, ip in enumerate(ips):
     processes.append(p)
 
 # wait and kill nodes
-time.sleep(150)
-print("Shutting down nodes..")
+time.sleep(10)
+print("Shutting down nodes...")
 for p in processes:
     p.kill()
 
 # just to be sure
 subprocess.run("killall -9 app", shell=True)
+
+print("Parsing logs...")
+print(f'Average time between two beacon values: {parse_files("logs/")}s')
